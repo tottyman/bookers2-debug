0b6e971dfaab71aa3959f4349f80f18bc75c38a9
ToshiyukiIsshiki <114905631+tottyman@users.noreply.github.com>
2022-11-20 06:24:00 +0000

    [finish] bookers2-debug

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def show
+    @book = Book.find(params[:id])
+    @bookn = Book.new
+    @user = @book.user
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    @books = current_user.books
+    @book = @books.find_by(id: params[:id])
+    redirect_to books_path unless @book
+  end
+  
+end
